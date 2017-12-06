4b0930fafbf8e3b1533df83387e57186085f7e30
sid-ghosh <34320334+sid-ghosh@users.noreply.github.com>
2017-12-06 15:56:22 -0600

    Some format change and betterment

--- AOCDay1.java
+++ AOCDay1.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class AOCDay1 {
 
-    public final static String PATH = "C:\\Users\\e882pv\\Desktop\\Day1AOC.txt";
+    private final static String PATH = "C:\\Users\\e882pv\\Desktop\\Day1AOC.txt";
 
 
     public static void main(String[] args) throws IOException {
@@ -15,63 +15,52 @@ public class AOCDay1 {
 
         String currentLine;
         String inputSequence = null;
-        int seqLength = 0;
+        int seqLength;
         int sum = 0;
-        int midLength = 0;
+        int midLength;
 
         try {
             while ((currentLine = bufferedReader.readLine()) != null) {
                 inputSequence = currentLine;
             }
 
-            seqLength = inputSequence.length();
-            midLength = seqLength / 2;
-            System.out.println("Length is " + seqLength);
-            System.out.println("Length is " + midLength);
+            if (inputSequence != null && !(inputSequence.equalsIgnoreCase(""))) {
 
-            int counter = 0;
+                seqLength = inputSequence.length();
+                midLength = seqLength / 2;
 
-            int[] numbers1 = new int[seqLength];
-            int[] numbers2 = new int[seqLength];
-            for (int i = 0; i < midLength; i++) {
-                numbers1[i] = Integer.parseInt(Character.toString(inputSequence.charAt(i)));
-                counter++;
-                //System.out.println("Digit @ position " + (i + 1) + " is " + numbers[i]);
-            }
-            System.out.println("counter is " + counter);
+                int counter = 0;
 
-            for (int i = 0; i < midLength; i++) {
-                numbers2[i] = Integer.parseInt(Character.toString(inputSequence.charAt(counter)));
-                counter++;
-                //System.out.println("Digit @ position " + (i + 1) + " is " + numbers[i]);
-            }
-            System.out.println("counter is " + counter);
-
-            counter = 0;
-
-
-            for (counter = 0; counter < midLength; counter++) {
-                if (numbers1[counter] == numbers2[counter]) {
-                    sum += numbers1[counter] + numbers2[counter];
-                } else {
-                    sum += 0;
+                int[] numbers1 = new int[seqLength];
+                int[] numbers2 = new int[seqLength];
+                for (int i = 0; i < midLength; i++) {
+                    numbers1[i] = Integer.parseInt(Character.toString(inputSequence.charAt(i)));
+                    counter++;
                 }
-            }
-            System.out.println("Sum is " + sum);
 
+                for (int i = 0; i < midLength; i++) {
+                    numbers2[i] = Integer.parseInt(Character.toString(inputSequence.charAt(counter)));
+                    counter++;
+                }
+
+
+                for (counter = 0; counter < midLength; counter++) {
+                    if (numbers1[counter] == numbers2[counter]) {
+                        sum += numbers1[counter] + numbers2[counter];
+                    } else {
+                        sum += 0;
+                    }
+                }
+                System.out.println("Sum is " + sum);
+            }
         } catch (IOException ex) {
-            ex.printStackTrace();
+            System.out.println("Input file not found");
         } finally {
             try {
-                if (bufferedReader != null)
-                    bufferedReader.close();
-
-                if (fileReader != null)
-                    fileReader.close();
-
+                bufferedReader.close();
+                fileReader.close();
             } catch (IOException ex) {
-
-                ex.printStackTrace();
+                System.out.println("Input file cannot be closed/found");
             }
         }
     }
